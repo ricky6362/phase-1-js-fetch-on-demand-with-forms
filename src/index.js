fbeb54940169b6245d9afa31878e323b5217790c
ricky6362 <63843083+ricky6362@users.noreply.github.com>
2023-06-27 13:44:41 +0300

    assignment done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,17 @@
 const init = () => {
-  
+    const form = document.querySelector('form')
+    form.addEventListener('submit', function(event){
+        event.preventDefault()
+        const searchByIdInput = document.getElementById("searchByID")
+        fetch(`http://localhost:3000/movies/${searchByIdInput.value}`)
+        .then(resp => resp.json())
+        .then(data => {
+            const title = document.querySelector('#movieDetails h4')
+            const summary = document.querySelector('#movieDetails p')
+            title.textContent = data.title
+            summary.textContent = data.summary
+        })
+    })
 }
-
-document.addEventListener('DOMContentLoaded', init);
+  
+  document.addEventListener('DOMContentLoaded', init);
